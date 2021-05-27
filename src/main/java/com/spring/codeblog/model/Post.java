a5830e37cfeb08fc3433f644a4ef9528c43d28a9
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-05-27 14:37:44 -0300

    implemented controller and view posts

--- src/main/java/com/spring/codeblog/model/Post.java
+++ src/main/java/com/spring/codeblog/model/Post.java
@@ -2,7 +2,6 @@ package com.spring.codeblog.model;
 
 import java.time.LocalDate;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +11,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
 
 
 @Entity
